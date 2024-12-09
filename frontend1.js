ea05e502ab12064bbe2cd0f8b73cc5e30d79dea0
soniadiblazee <cindylol.11037@gmail.com>
2024-12-09 08:17:11 -0500

    changed message and file name

--- frontend1.js
+++
@@ -1,7 +0,0 @@
-/* >code for frontend goes here.
->repository was created for future use when
-i work on this at home.
->MVP goes here .
-*/
-
-// this will suck BOOTYCHEEKS
---
+++ messageAndCredits.js
@@ -0,0 +1,5 @@
+/* >MVP goes in this sepreate repository until (fully) completed .
+>frontend by Sonia DiBlasio
+*/
+
+// this will suck BOOTYCHEEKS
