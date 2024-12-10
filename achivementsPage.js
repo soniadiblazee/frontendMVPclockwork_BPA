889f1a0ba55712d6016282ae91256a97822f4ca0
soniadiblazee <cindylol.11037@gmail.com>
2024-12-10 09:45:05 -0500

    name change (charts.js => chartsPage.js); achivements page

---
+++ achivementsPage.js
@@ -0,0 +1,22 @@
+// import react \/
+import React from react;
+
+/* a list of achivements that will be displayed
+two achivements are here as examples of what kinds of achivements could be available
+ more achivements can be added */
+
+function achivementsPage () {
+    return (
+        <div>
+            <h1>Your Achivements</h1>
+            <ul>
+                <li>Complete 5 goals</li>
+                <li>Login - 30 day streak</li>
+                {/* list of achivements */}
+            </ul>
+        </div>
+    )
+};
+
+export default achivementsPage
+//export default so this code can be used in other files
