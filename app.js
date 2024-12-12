9a061b20a930ee80ae9892fe98ad927cf6019a40
soniadiblazee <cindylol.11037@gmail.com>
2024-12-12 09:54:25 -0500

    app.js update, more to come since i need to but these into src so js can be linked to html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // import react; react-router-dom
 import React from 'react';
-import {BrowswerRouter as Router,Route,Switch} from 'react-router-dom';
+import {BrowswerRouter as Router,Route,Switch,Link} from 'react-router-dom';
 
 // import all previously made pages
 import LoginPage from './pages/loginPage';
@@ -8,20 +8,34 @@ import AchivementsPage from './pages/achivementsPage';
 import ChartsPage from './pages/chartsPage';
 import ChatbotPage from './pages/chatbotPage';
 import GoalsPage from './pages/goalsPage';
-import ProgressPage from './pages/progressPage'
+import ProgressPage from './pages/progressPage';
 
 //function for switching/navigating between the pages of the app
 function App() {
     return (
         <Router>
-            <Switch>
-                <Route path="/login" compodent={LoginPage} />
-                <Route path="/goals" compodent={GoalsPage} />
-                <Route path="/achivements" compodent={AchivementsPage} />
-                <Route path="/charts" compodent={ChartsPage} />
-                <Route path="/chatbot" compodent={ChatbotPage} />
-                <Route path="/progress" compodent={ProgressPage} />
-            </Switch>
+            <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>Home
+                        <li><Link to="/login">Home</Link></li>
+                        <li><Link to="/achievments">Achievments</Link></li>
+                        <li><Link to="/charts">Charts</Link></li>
+                        <li><Link to="/chatbot">Chatbot</Link></li>
+                        <li><Link to="/goals">Goals</Link></li>
+                        <li><Link to="/progress">Progress</Link></li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/login" compodent={LoginPage}/>
+                    <Route path="/achievments" compodent={AchivementsPage}/>
+                    <Route path="/charts" compodent={ChartsPage}/>
+                    <Route path="/chatbot" compodent={ChatbotPage}/>
+                    <Route path="/goals" compodent={GoalsPage}/>
+                    <Route path="/progress" compodent={ProgressPage}/>
+                </Switch>
+            </div>
         </Router>
     );
 }
